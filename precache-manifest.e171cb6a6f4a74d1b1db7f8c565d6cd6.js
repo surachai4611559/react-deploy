d11d3a3f2fdbce898ea8e990c18c26a7f54cd35f
surachai4611559 <sukoongsati@gmail.com>
2020-04-19 08:42:26 +0700

    Updates

---
+++ precache-manifest.e171cb6a6f4a74d1b1db7f8c565d6cd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a78763462da3dc07cd628767e8fe0c",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "d81d967671927363ea97",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "30d0eda4c6200fc3652c",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81d967671927363ea97",
+    "url": "/react-deploy/static/js/main.9b47abb7.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
